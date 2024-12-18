d9de9c4c3d09bb07159d31d1e31cf385cf5d2660
Kamil C <kamil.cylko@gmail.com>
2024-12-18 15:39:46 +0100

    init

---
+++ app/region-routes/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function RegionRoutes() {
+  return (
+    <div>
+      <Link href='./'>Home</Link>
+      <div>Region Routes</div>
+    </div>
+  );
+}
